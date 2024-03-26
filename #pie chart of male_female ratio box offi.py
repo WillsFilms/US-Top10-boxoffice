2aeca31df1dacff3593ad6d6b35f085efc7d4a8b
WillsFilms <163652601+WillsFilms@users.noreply.github.com>
2024-03-26 21:32:16 +0000

    added percentages and a legend

--- #pie chart of male_female ratio box offi.py
+++ #pie chart of male_female ratio box offi.py
@@ -1,11 +1,9 @@
 #pie chart of male_female ratio box office top 10
 
 #make packages available
-import ast 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 #import the data
 Top_10 = pd.read_csv("C:\\Users\\HP\\Documents\\Data Analysis\\Wills Films\\Kaggle datasets\\Box_Office_top_10_2000_2023.csv")
@@ -26,13 +24,15 @@ explode = (0.0, 0.2)
 wp = {'linewidth':1, 'edgecolor': "black"}
 
 #plot pie chart
-plt.pie(sizes, explode=explode, 
-        labels=director_gender, 
+plt.pie(sizes, explode=explode,  
         colors= ("#0070C0","#F612DB"),
-        autopct=None, 
+        autopct='%1.1f%%', 
         shadow=True, 
         wedgeprops=wp)
-plt.title('Proportion of male to female directors')
+plt.title('Proportion of male to female directors of US top 10 box office hits; 2000-2023')
+plt.legend(labels=director_gender, fontsize="18", loc="best")
 
-#show plot
+#fine tune display and show plot
+plt.axis('equal')
+plt.tight_layout()
 plt.show()
